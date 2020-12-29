7bf22108b04c9ef9351c81126ff68ac804a4720c
ravinandan25 <ravinandan.bhardwaj@gmail.com>
2020-12-29 23:07:14 +0530

    commit

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,5 +1,25 @@
 public class StringCalculator {
     public int Add(String numbers){
-        return 0;
+        String[] arrays_number=numbers.split(",");
+       if(numbers.isEmpty()){
+           return 0;
+       }
+       if(numbers.length()==1){
+           return stringToInt(numbers);
+       }
+       else{
+           return sumof2numbers(arrays_number[0],arrays_number[1]);
+
+       }
+
+
     }
+    private int stringToInt(String numbers){
+        return Integer.valueOf(numbers);
+    }
+    private int sumof2numbers(String number1 ,String number2){
+        return Integer.valueOf(number1)+Integer.valueOf(number2);
+    }
+
 }
+
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,10 +1,25 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
-    StringCalculator sc=new StringCalculator();
+    private StringCalculator sc;
+    @Before
+    public void init(){
+
+        sc=new StringCalculator();
+    }
     @Test
     public void emptyinput(){
+
         Assert.assertEquals(0,sc.Add(""));
+    }
+    @Test
+    public void input1number(){
+        Assert.assertEquals(1,sc.Add("1"));
+    }
+    @Test
+    public void input2number(){
+        Assert.assertEquals(3,sc.Add("1,2"));
     }
 }
